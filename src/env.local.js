0eab62a6e080c663a10ef9581f34a7cf16bf61cd
cnietocu <cnietocu@gmail.com>
2026-08-25 10:28:10 -0300

    Update seller API

--- src/env.local.js
+++ src/env.local.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // TODO: generate a new AppKey/AppToken pair for the "pocicbcmall" VTEX account
-  // (License Manager > Account Settings > App Keys) — the old pocsanbesfera pair
-  // below is a different account's credentials and will not authenticate here.
-  appKey: 'REPLACE_WITH_POCICBCMALL_APPKEY',
-  appToken: 'REPLACE_WITH_POCICBCMALL_APPTOKEN',
+  appKey: 'vtexappkey-pocicbcmall-QNVRIJ',
+  appToken:
+    'FEGQKCGROPMZOMLFUZSLURPBSNBEDRHIYLULVISVXMNJFNQAIBIWQWYOKZAKNXTHZNOPCTZYRWTFCANCDNLYDWSCJDTKYMDGVSCWMVMAPWIPZXPFTBEEIPNVSDKJBADU',
 }
--- src/graphql/thirdParty/helpers/index.ts
+++ src/graphql/thirdParty/helpers/index.ts
@@ -3,9 +3,9 @@ export const getRequestInit = (method = 'GET', headers = {}): RequestInit => ({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    // TODO: same new "pocicbcmall" AppKey/AppToken pair as src/env.local.js
-    'X-VTEX-API-AppKey': 'REPLACE_WITH_POCICBCMALL_APPKEY',
-    'X-VTEX-API-AppToken': 'REPLACE_WITH_POCICBCMALL_APPTOKEN',
+    'X-VTEX-API-AppKey': 'vtexappkey-pocicbcmall-QNVRIJ',
+    'X-VTEX-API-AppToken':
+      'FEGQKCGROPMZOMLFUZSLURPBSNBEDRHIYLULVISVXMNJFNQAIBIWQWYOKZAKNXTHZNOPCTZYRWTFCANCDNLYDWSCJDTKYMDGVSCWMVMAPWIPZXPFTBEEIPNVSDKJBADU',
     ...headers,
   },
 })
